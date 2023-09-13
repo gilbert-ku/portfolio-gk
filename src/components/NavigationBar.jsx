b78d668d96f8ad2485c2bb1de30f1915b2dad2be
gilbert-ku <gilbert45ku@gmail.com>
2023-09-14 01:49:34 +0300

    add navigation bar

---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,37 @@
+
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import React from "react";
+import Home from  "./Home"
+import Portfolio from "./Portfolio"
+import Blog from "./Blog"
+import Contact from "./Contact"
+
+function NavBar() {
+    return (
+        <>
+            <BrowserRouter>
+                <nav>
+                    <h1>gilbert~ku</h1>
+                </nav>
+                <ul>
+                    <li><Link to="/"> Home</Link></li>
+                    <li><Link to="/Portfolio">Portfolio</Link></li>
+                    <li><Link to="/Blog">Blog</Link></li>
+                    <li><Link to="/Contact">Contact</Link></li>
+                </ul>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/Portfolio" element={< Portfolio/>} />
+                    <Route path="/Blog" element={<Blog />} />
+                    <Route path="/Contact" element={<Contact />} />
+                </Routes>
+            </BrowserRouter>
+            {/* <Footer>
+                <h3>copyright gilbert-ku last update - 2023</h3>
+            </Footer> */}
+        </>
+    )
+
+}
+
+export default NavBar;
