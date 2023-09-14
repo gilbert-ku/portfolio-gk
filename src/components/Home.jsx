a71d75192b1b4471c857e224b8cf1e05ad49dc99
gilbert-ku <gilbert45ku@gmail.com>
2023-09-14 21:46:19 +0300

    add proficient

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -23,19 +23,30 @@ function Home(){
                 
             </div>
 
-            <div className="social-media">
-                <h2>Let's connect</h2>
-                <ul className="icons">
-                    <li><a href="https://www.linkedin.com/in/gilbert-kutoto/"><img src="/images/pngtree-linked-in-icon-png-image_3584840.jpg" alt="linkedin"  width="50px"/></a></li>
-                    <li><a href="https://github.com/gilbert-ku"><img src="/images/githuh.png" alt="Github"  width="50px"/></a></li>
-                    <li><a href="https://twitter.com/gilbert45dope/"><img src="/images/x.jpeg" alt="twitter"  width="50px"/></a></li>
-                    <li><a href="https://wa.me/+254726545820"><img src="/images/whatsApp.jpg" alt="WhatsApp"  width="50px"/></a></li>
-                </ul>
+
+            <div links-to-media>
+                <div className="social-media">
+                    <h2>Let's connect</h2>
+                    <ul className="icons">
+                        <li><a href="https://www.linkedin.com/in/gilbert-kutoto/"><img src="/images/pngtree-linked-in-icon-png-image_3584840.jpg" alt="linkedin"  width="50px"/></a></li>
+                        <li><a href="https://github.com/gilbert-ku"><img src="/images/githuh.png" alt="Github"  width="50px"/></a></li>
+                        <li><a href="https://twitter.com/gilbert45dope/"><img src="/images/x.jpeg" alt="twitter"  width="50px"/></a></li>
+                        <li><a href="https://wa.me/+254726545820"><img src="/images/whatsApp.jpg" alt="WhatsApp"  width="50px"/></a></li>
+                    </ul>
+                </div>
+                <div className="coding-tools">
+                    <h2>Proficient in </h2>
+                    <ul className="coding">
+                        <li><img src="/images/front.png" alt="linkedin"  width="50px"/></li>
+                        <li><img src="/images/react.png" alt="React"  width="50px"/></li>
+                        <li><img src="/images/python.png" alt="twitter"  width="50px"/></li>
+                        <li><img src="/images/sql.jpeg" alt="WhatsApp"  width="50px"/></li>
+                        <li><img src="/images/flask-python.png" alt="twitter"  width="50px"/></li>
+                        <li><img src="/images/Figma-1-logo.png" alt="WhatsApp"  width="50px"/></li>
+                    </ul>
+                </div>
             </div>
 
-               
-            
-            
         </>
         
     )
