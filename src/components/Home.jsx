940e729a789d851c38acb147acd79d8d6b31ab40
gilbert-ku <gilbert45ku@gmail.com>
2023-09-14 16:42:13 +0300

    create links to icon

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -23,6 +23,15 @@ function Home(){
                 
             </div>
 
+            <div className="social-media">
+                <h2>Let's connect</h2>
+                <ul>
+                    <li><a href="https://www.linkedin.com/in/gilbert-kutoto/"><img src="/images/pngtree-linked-in-icon-png-image_3584840.jpg" alt="linkedin"  width="50px"/></a></li>
+                    <li><a href="https://github.com/gilbert-ku"><img src="/images/githuh.png" alt="Github"  width="50px"/></a></li>
+                    <li><a href="https://twitter.com/gilbert45dope/"><img src="/images/x.jpeg" alt="twitter"  width="50px"/></a></li>
+                    <li><a href="https://wa.me/+254726545820"><img src="/images/whatsApp.jpg" alt="WhatsApp"  width="50px"/></a></li>
+                </ul>
+            </div>
 
                
             
