cb5bfb9e21d57bf4dd1fef9d1e304bbaa6c09c45
gilbert-ku <gilbert45ku@gmail.com>
2023-09-16 11:15:30 +0300

    add feedback form

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,12 +1,28 @@
-function Portfolio(){
+import React, { useState } from 'react';
+
+function Portfolio() {
+  // State to track the selected feedback type
+  const [feedbackType, setFeedbackType] = useState('anonymous');
+
+  // State to track the visibility of the name input
+  const [isNameVisible, setIsNameVisible] = useState(false);
+
+  // Function to handle changes in the feedback type dropdown
+  const handleFeedbackTypeChange = (event) => {
+    const selectedType = event.target.value;
+    setFeedbackType(selectedType);
+
+    // Show/hide the name input based on the selected feedback type
+    setIsNameVisible(selectedType === 'named');
+  };
     return (
         <div className="main-portfolio">
-            <p>Portfolio</p>
-                <h1>Welcom to my Portfolio</h1>
+            <p className="p-P">Portfolio</p>
+                <h1 className="h-H">Welcome to my Portfolio</h1>
                     <div className="cards">
                         <img src="/images/Screenshot from 2023-09-15 13-32-57.png" alt="School Home Page" width="600px"/>
                             <div className="card-description">
-                                <h1><a href="https://github.com/gilbert-ku/phase-1-one--school_project-">School Website</a></h1>
+                                <h1 className="header-link"><a href="https://github.com/gilbert-ku/phase-1-one--school_project-">School Website</a></h1>
                                     <p>Our International School's website is a dynamic online platform that reflects our commitment to providing an outstanding educational experience. Built using modern web technologies, our website leverages HTML, CSS, JavaScript, and JSON to offer a user-friendly and engaging browsing experience.</p>
                                         <h3>Here's a brief description of the technologies used:</h3>
                                             <ol>
@@ -23,7 +39,7 @@ function Portfolio(){
                        <div className="cards">
                         
                             <div className="card-description">
-                                <h1> <a href="https://github.com/gilbert-ku/Go-Trave">Go Travel</a></h1>
+                                <h1 className="header-link"> <a href="https://github.com/gilbert-ku/Go-Trave">Go Travel</a></h1>
                                     <p>GOTRAVEL is a tourism-centric project developed using the React programming language. It serves as a one-stop platform dedicated to enhancing the travel experience for both tourists and local tourism operators. Our mission is to simplify trip planning by providing reliable information on tourist services, attractions, and sceneries, filling the void left by major websites like TripAdvisor.</p>
                                         <h3>Key features of GOTRAVEL include:</h3>
                                             <ol>
@@ -42,7 +58,7 @@ function Portfolio(){
                         <div className="cards">
                         <img src="/images/Screenshot from 2023-09-15 13-39-03.png" alt="school database" width="600px"/>
                             <div className="card-description">
-                                <h1><a href="https://github.com/gilbert-ku/schooldb-cli-project"> School Management CLI App with Python and SQLAlchemy</a></h1>
+                                <h1 className="header-link"><a href="https://github.com/gilbert-ku/schooldb-cli-project"> School Management CLI App with Python and SQLAlchemy</a></h1>
                                     <p>Welcome to the School Management CLI App! This command-line application, built using Python and SQLAlchemy, empowers you to efficiently manage student and teacher records, courses, enrollments, and grades within a school database.</p>
                                         <h3>Features:</h3>
                                             <ol>
@@ -57,8 +73,47 @@ function Portfolio(){
                             </div>
                     </div>
 
-            
+    <div className="feedback-form">
+        <form action="" method="post">
+          {/* Dropdown for feedback type */}
+          <label htmlFor="feedback-type">Select Feedback Type:</label>
+          <select
+            name="feedback-type"
+            id="feedback-type"
+            value={feedbackType}
+            onChange={handleFeedbackTypeChange}
+          >
+            <option value="anonymous">Send anonymous feedback</option>
+            <option value="named">Enter Your Name</option>
+          </select>
+          {/* <br /> */}
+
+          {/* Input field for the name (only shown if "Enter Your Name" is selected) */}
+          {isNameVisible && (
+            <div>
+              <label htmlFor="name">Your Name:</label>   
+              {/* <br /> */}
+              <input type="text" id="name" name="name" placeholder="Enter your name" />
+            </div>
+          )}
+
+          {/* Textarea for feedback message */}
+          {/* <label htmlFor="feedback-message">Feedback:</label> */}
+          <textarea
+            id="feedback-message"
+            name="feedback-message"
+            placeholder="Message"
+            rows="4"
+            required
+          ></textarea>
+
+          {/* Submit button */}
+          <br />
+          <input type="submit" value="Submit Feedback" className="feedbackbtn"/>
+        </form>
+    </div>
         </div>
+        
         
     )
 }
